a4b67d9619da73c87f222c2d56ca288a4e4ba1a6
rahul kore <rahulkore976@gmail.com>
2022-07-21 00:26:16 +0530

    initial commit

---
+++ Master-class-repo/UI/Angular/e2e/src/app.po.ts
@@ -0,0 +1,20 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo(link: string) {
+    return browser.get(link);
+  }
+
+  getParagraphText(selector) {
+    return element(by.css(selector)).getText();
+  }
+	
+	
+  getElement(selector: string) {
+	  return element(by.css(selector));
+  }
+	
+  getAllElements(selector: string) {
+	  return element.all(by.css(selector));
+  }
+}
